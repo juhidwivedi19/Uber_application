32f2cd0519fc0681ef3d74b69e88a39f8b0c6cbb
Juhi <257458510+juhidwivedi19@users.noreply.github.com>
2026-05-26 23:53:10 +0530

    Updated frontend and middleware changes

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const blackListTokenModel = require('../models/blackListToken.model');
-
+const captainModel = require('../models/captain.model');
 
 module.exports.authUser = async (req,res,next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
@@ -39,12 +39,15 @@ module.exports.authUser = async (req,res,next) => {
 module.exports.authCaptain = async (req,res,next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
+    console.log(token);
+
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
      const isBlacklisted = await blackListTokenModel.findOne({ token: token });
    
+
      if(isBlacklisted) {
         return res.status(401).json({ message: 'Invalid token.' });
      }
@@ -54,7 +57,9 @@ module.exports.authCaptain = async (req,res,next) => {
          const captain = await blackListTokenModel.findById(decoded._id);
 
          req.captain = captain;  //In request.captain we store the captain information
+         return next()  //next() is used to move to the next middleware or route handler
     } catch (error) {
+        console.log(error);
         return res.status(401).json({ message: 'Invalid token.' });
     }
 }
